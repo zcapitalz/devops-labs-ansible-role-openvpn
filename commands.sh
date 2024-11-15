c761e8f45b10bb8c42cca0896ba698a3b9d2ea7a
zcapitalz <zadneprovskiywork@gmail.com>
2024-11-15 15:05:28 +0300

    refactor: delete unneeded files

--- commands.sh
+++
@@ -1,3 +0,0 @@
-molecule init scenario --driver-name docker run_openvpn_server
-pipx install --force --include-deps molecule-plugins[vagrant]@git+https://github.com/ansible-community/molecule-plugins.git
-molecule login --host openvpn-server -s run_openvpn_server
